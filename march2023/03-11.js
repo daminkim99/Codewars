0fbd8d05868f507275f61d6790241f145a691709
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-11 19:10:18 -0400

    update 03-11.js solutions

--- march2023/03-11.js
+++ march2023/03-11.js
@@ -1,3 +1,26 @@
 //3.11.2023 7kyu codewars 
 
-//6. 
+//6. Given an integral number, determine if it's a square number
+ var isSquare = n => Number.isInteger(Math.sqrt(n))
+
+
+ //7. function that can take any non-negative integer as an argument and return it with its digits in descending order. 
+ 
+ function descendingOrder(n){
+    arrN= n.toString().split('')
+    return Number(arrN.sort((a,b) => b-a).join(''))
+  }
+  
+//8. Implement a function that determines whether a string that contains only letters is an isogram. 
+  function isIsogram(str){
+    str = str.toLowerCase()
+    let compareStr = ''
+    for (let i = 0 ; i< str.length; i++){
+      if (str[i]=== ''){
+          return true
+      } else if(!compareStr.includes(str[i])){
+        compareStr +=str[i]
+      }
+    } console.log(compareStr)
+    return compareStr == str? true:false
+  }
