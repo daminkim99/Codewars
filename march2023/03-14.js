3ccff885a8793aadd51d94a10086414ac4f3b8f2
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-14 23:18:58 -0300

    update  03-14.js

--- march2023/03-14.js
+++ march2023/03-14.js
@@ -1,3 +1,6 @@
 //3.14.23 
 
-//
+//14. You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+function check(a, x) {
+    return a.includes(x)
+  }
