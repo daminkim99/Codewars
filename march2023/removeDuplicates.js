1cf2c450323f647ecf29cf2d10e37bbb90c2ca13
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-26 13:53:01 -0300

    removes duplicates in a string

---
+++ march2023/removeDuplicates.js
@@ -0,0 +1,22 @@
+/* 03.26.23 
+
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+P: string of words 
+R: one string with no repeats 
+E: 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta' -> alpha beta gamma delta'alpha beta gamma delta'
+*/
+function removeDuplicateWords (s) {
+    //split it into an array of words 
+      let arrS= s.split(' ')
+      let sorted=[]
+    //filter so that only the words that don't exist in an array is returned 
+     for (let i =0; i<arrS.length; i++){
+       if (!sorted.includes(arrS[i])){
+         sorted.push(arrS[i])
+       }
+     }
+    return sorted.join(' ')
+  }
+
+  
