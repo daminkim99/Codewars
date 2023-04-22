4c3d070b8935cfab0d3346eaa8f9de30388e0319
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-04-21 23:17:14 -0300

    converts a string to uppercase

---
+++ april2023/makeUpperCase.js
@@ -0,0 +1,13 @@
+/* 4.21.23 
+
+Write a function which converts the input string to uppercase.
+
+P: Input of string 
+R: returns an uppercase of a string 
+E: 'hello' --> 'HELLO' 
+*/ 
+
+function makeUpperCase(str) {
+    //make use of toUpperCase method 
+    return str.toUpperCase()
+  }
