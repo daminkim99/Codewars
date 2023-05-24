76f8e7cc5a3a61981e3f29fbba9920deb3b45919
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-05-24 13:19:01 -0300

    add a property to the objects by series of concatenation

---
+++ may2023/higherOrderFunctionSeries10.js
@@ -0,0 +1,22 @@
+/* 5.24.23 
+
+write a function that adds the username property to each object in the input array:
+
+The value of the username property is composed by concatenating:
+
+firstName in lower-case;
+first letter of the lastName in lower-case; and
+the birth year which for the purpose of this kata is calculated simply by subtracting age from the current year. Please make sure that your function delivers the correct birth year irrespective of when it will be executed, for example it should also work correctly for a meetup organised in 10-years-time. The example above assumes that the function is run in year 2020.
+*/
+
+
+
+
+
+function addUsername(list) {
+    //concatenation via template literals 
+    list.forEach(x => x['username']= `${x.firstName.toLowerCase()}${x.lastName[0].toLowerCase()}${new Date().getFullYear()- x.age}`)
+   
+    return list
+  }
+  
